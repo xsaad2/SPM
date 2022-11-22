4fb846eaeeb03be92be67626895858cbbe779ff0
Saad Belkhou <saad.belkhou@gmail.com>
2022-11-22 04:02:33 +0100

    created EvalRecords Model

---
+++ backend/src/models/EvalRecord.js
@@ -0,0 +1,11 @@
+class SalesMan{
+    constructor(desc, year,targetValue, actualValue) {
+        this._id = undefined;
+        this.desc = desc;
+        this.year = year;
+        this.targetValue = targetValue;
+        this.actualValue=actualValue;
+    }
+}
+
+module.exports = SalesMan;
